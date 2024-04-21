e2da2c8bfcc29187fc2e7bf5b6028131a3ceece4
integer <7279624+integerrr@users.noreply.github.com>
2024-04-22 00:10:35 +0100

    Testing new coop codes

--- leaderboard/src/main.rs
+++ leaderboard/src/main.rs
@@ -14,20 +14,32 @@ async fn main() -> Result<()> {
         .connect("postgres://localhost:5432/whal-int-test")
         .await?;
 
-    let contract_id = "Luncheggbles-2022";
-    let coop_ids = vec!["regime813", "father747", "degree267", "holyshit"];
+    let ultra_contract_id = "Luncheggbles-2022";
+    let fr_coop_ids = vec!["regime813", "father747", "degree267", "holyshit"];
+    let usr_coop_ids = vec!["manila784"];
 
     let mut fr = ActiveContractBuilder::new()
-        .with_contract_id(contract_id)
+        .with_contract_id(ultra_contract_id)
         .with_coop_flag(CoopFlag::Fastrun)
         .with_pg_pool(pgpool.clone())
         .build()
         .await?;
 
-    let _ = fr.add_coops(&coop_ids).await;
+    let mut usr = ActiveContractBuilder::new()
+        .with_contract_id(ultra_contract_id)
+        .with_coop_flag(CoopFlag::Speedrun)
+        .with_pg_pool(pgpool.clone())
+        .build()
+        .await?;
 
-    let mut sr_table = SrucTable::new();
-    sr_table.add_data_rows(fr.coops().as_slice());
+    let _ = fr.add_coops(&fr_coop_ids).await;
+    let _ = usr.add_coops(&usr_coop_ids).await;
+
+    let mut fr_table = SrucTable::new();
+    fr_table.add_data_rows(fr.coops().as_slice());
+
+    let mut usr_table = SrucTable::new();
+    usr_table.add_data_rows(usr.coops().as_slice());
 
     Ok(())
 }
